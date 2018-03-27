e3dac505f3dca1a7d113726eb730a0dcce65c164
Kyle Harris <k_harris95@live.com>
2018-03-27 20:07:50 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all 
+    @@all.each do |dog_name|
+      puts dog_name
+    end
+  end
+end
