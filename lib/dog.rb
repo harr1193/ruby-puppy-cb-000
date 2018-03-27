3ce6f2bd21ce7f42124c4f725195b01e5ec6ad12
Kyle Harris <k_harris95@live.com>
2018-03-27 20:10:25 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,8 +7,12 @@ class Dog
   end
 
   def self.all
-    @@all.each do |dog_name|
-      puts dog_name
+    @@all.each do |name|
+      puts name
     end
+  end
+
+  def self.clear_all
+    @@all.clear
   end
 end
